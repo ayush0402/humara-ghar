3f9f3152cf8e6043044194438ba10e63f441177a
Ayush Kumar <ayushkumar0402@gmail.com>
2023-10-28 20:45:40 +0530

    removed template code

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -14,7 +14,7 @@ export default async function AuthButton() {
     <div className="flex items-center gap-4">
       Hey, {user.email}!
       <form action="/auth/sign-out" method="post">
-        <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
+        <button className="py-2 px-4 rounded-md no-underline">
           Logout
         </button>
       </form>
@@ -22,7 +22,7 @@ export default async function AuthButton() {
   ) : (
     <Link
       href="/login"
-      className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+      className="py-2 px-3 flex rounded-md no-underline"
     >
       Login
     </Link>
