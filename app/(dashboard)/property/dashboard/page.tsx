1804c605888ebc2d2a6e1196bb4c7eb6b4a17276
Ayush Kumar <ayushkumar0402@gmail.com>
2023-11-14 00:15:46 +0530

    added supabase initial migration

--- app/(dashboard)/property/dashboard/page.tsx
+++ app/(dashboard)/property/dashboard/page.tsx
@@ -1,61 +1,60 @@
-import RoommateCard from '@/components/custom/roommate-card';
-import { Navbar } from '@/components/navbar-owner';
-import PropertyCard from '@/components/property-card';
-import { Sidebar } from '@/components/sidebar';
-import { Button } from '@/components/ui/button';
-import { PlusIcon } from '@radix-ui/react-icons';
-import Link from 'next/link';
-import React from 'react'
-const page = () => {
-  
-  return (
-    <div className='flex space-x-4 space-y-2'>
-      <div className='flex flex-wrap'>
-      <RoommateCard
-                    imageSrc="https://picsum.photos/200"
-                    name="Listing Name"
-                    location="Listing Location"
-                    rentAmount={10000}
-                    lookingForGender="male"
-                    lookingForType="roommate"
-                    matchPercentage={80}
-                />
-      <RoommateCard
-                    imageSrc="https://picsum.photos/200"
-                    name="Listing Name"
-                    location="Listing Location"
-                    rentAmount={10000}
-                    lookingForGender="male"
-                    lookingForType="roommate"
-                    matchPercentage={80}
-                />
-      <RoommateCard
-                    imageSrc="https://picsum.photos/200"
-                    name="Listing Name"
-                    location="Listing Location"
-                    rentAmount={10000}
-                    lookingForGender="male"
-                    lookingForType="roommate"
-                    matchPercentage={80}
-                />
-      <RoommateCard
-                    imageSrc="https://picsum.photos/200"
-                    name="Listing Name"
-                    location="Listing Location"
-                    rentAmount={10000}
-                    lookingForGender="male"
-                    lookingForType="roommate"
-                    matchPercentage={80}
-                />
-      </div >
-      <div className='mr-[40px]'>
-    <Link href="/property/create"><Button variant="default" className='mr-[40px]'>
-          <PlusIcon className='h-5 w-5'/>
-           Add a property
-    </Button></Link>
-    </div>
-    </div>
-  )
-}
+import RoommateCard from "@/components/custom/roommate-card";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+import React from "react";
 
-export default page
+const page = () => {
+  return (
+    <div className="flex space-x-4 space-y-2">
+      <div className="flex flex-wrap">
+        <RoommateCard
+          imageSrc="https://picsum.photos/200"
+          name="Listing Name"
+          location="Listing Location"
+          rentAmount={10000}
+          lookingForGender="male"
+          lookingForType="roommate"
+          matchPercentage={80}
+        />
+        <RoommateCard
+          imageSrc="https://picsum.photos/200"
+          name="Listing Name"
+          location="Listing Location"
+          rentAmount={10000}
+          lookingForGender="male"
+          lookingForType="roommate"
+          matchPercentage={80}
+        />
+        <RoommateCard
+          imageSrc="https://picsum.photos/200"
+          name="Listing Name"
+          location="Listing Location"
+          rentAmount={10000}
+          lookingForGender="male"
+          lookingForType="roommate"
+          matchPercentage={80}
+        />
+        <RoommateCard
+          imageSrc="https://picsum.photos/200"
+          name="Listing Name"
+          location="Listing Location"
+          rentAmount={10000}
+          lookingForGender="male"
+          lookingForType="roommate"
+          matchPercentage={80}
+        />
+      </div>
+      <div className="mr-[40px]">
+        <Link href="/property/create">
+          <Button variant="default" className="mr-[40px]">
+            <PlusIcon className="h-5 w-5" />
+            Add a property
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- app/(dashboard)/roommate/page.tsx
+++ app/(dashboard)/roommate/page.tsx
@@ -1,53 +1,48 @@
-import RoommateCard from '@/components/custom/roommate-card'
-import { buttonVariants } from '@/components/ui/button'
-import Link from 'next/link'
-import React from 'react'
+import RoommateCard from "@/components/custom/roommate-card";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
 import { AiOutlinePlus } from "react-icons/ai";
 
-const page = () => {
-    return (
-        <div className='flex h-full w-full'>
-            <div className='flex flex-wrap'>
-                <RoommateCard
-                    imageSrc="https://picsum.photos/200"
-                    name="Listing Name"
-                    location="Listing Location"
-                    rentAmount={10000}
-                    lookingForGender="male"
-                    lookingForType="roommate"
-                    matchPercentage={80}
-                />
-                <RoommateCard
-                    imageSrc="https://picsum.photos/200"
-                    name="Listing Name"
-                    location="Listing Location"
-                    rentAmount={10000}
-                    lookingForGender="male"
-                    lookingForType="roommate"
-                    matchPercentage={80}
-                />
-                <RoommateCard
-                    imageSrc="https://picsum.photos/200"
-                    name="Listing Name"
-                    location="Listing Location"
-                    rentAmount={10000}
-                    lookingForGender="male"
-                    lookingForType="roommate"
-                    matchPercentage={80}
-                />
+const RoommatesPage = async () => {
+  // contains listings under room-required table.
+  // == listings of roommates.
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+  const { data: rooms } = await supabase
+    .from("room_required_listings")
+    .select("*");
+  return (
+    <div className="flex h-full w-full">
+      <div className="flex flex-wrap">
+        {!rooms ? (
+          <div>No rooms found.</div>
+        ) : (
+          rooms.map((room) => (
+            <RoommateCard
+              key={room.id} // assuming each room has a unique id
+              imageSrc="https://picsum.photos/200"
+              name="Listing Name"
+              location={room.location}
+              rentAmount={room.approx_rent}
+              lookingForGender={room.looking_for_gender}
+              lookingForType="roommate"
+              matchPercentage={80}
+            />
+          ))
+        )}
+      </div>
+      <div className="mx-2 mr-[30px] my-2">
+        <Link
+          href="/roommate/listing"
+          className={buttonVariants({ variant: "default" })}
+        >
+          <AiOutlinePlus className="pr-2" size={20} /> Add Listing
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-
-            </div>
-        <div className='mx-2 mr-[30px] my-2'>
-            <Link
-                href="/roommate/listing"
-                className={buttonVariants({ variant: "default" })}
-            >
-                <AiOutlinePlus className="pr-2" size={20} /> Add Listing
-            </Link>
-        </div>
-        </div>
-    )
-}
-
-export default page
+export default RoommatesPage;
