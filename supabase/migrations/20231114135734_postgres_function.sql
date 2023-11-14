974fe1600887085232e99365e828cbd1b8e55b7f
Ayush Kumar <ayushkumar0402@gmail.com>
2023-11-14 19:28:58 +0530

    added postgres function migration

---
+++ supabase/migrations/20231114135734_postgres_function.sql
@@ -0,0 +1,31 @@
+alter table "public"."room_required_listings" add column "creator_name" text default get_user_name_v2();
+
+alter table "public"."roommate_required_listings" add column "creator_name" text default get_user_name_v2();
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.get_user_name(input_user_id uuid)
+ RETURNS text
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+    user_name TEXT;
+BEGIN
+    SELECT name INTO user_name FROM user_profiles WHERE user_profiles.user_id = input_user_id;
+    RETURN user_name;
+END; $function$
+;
+
+CREATE OR REPLACE FUNCTION public.get_user_name_v2()
+ RETURNS text
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+    user_name TEXT;
+BEGIN
+    SELECT name INTO user_name FROM user_profiles WHERE user_profiles.user_id = auth.uid();
+    RETURN user_name;
+END; $function$
+;
+
+
