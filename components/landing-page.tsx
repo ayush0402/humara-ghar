8f9a327062cf3b45fcb2ad91030b17213b0f5daf
Akshay Bhatnagar <76173232+Astrra@users.noreply.github.com>
2023-11-17 18:49:29 +0530

    rectified bugs at landing page

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -6,13 +6,27 @@ import { Button } from "./ui/button";
 import FooterLanding from "./footer-landing";
 import Image from "next/image";
 import NavbarLanding from "./navbar-landing";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
 
 const font = Poppins({
   weight: "600",
   subsets: ["latin"],
 });
 
-const LandingPage = () => {
+const LandingPage = async () => {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  let redirect='/login';
+
+  if(user){
+    redirect='/home';
+  }
   return (
     <div className="h-full w-full bg-secondary">
       <NavbarLanding />
@@ -32,7 +46,7 @@ const LandingPage = () => {
           <div className="text-2xl mt-[5px]">
             perfect way to find your perfect roommates.
           </div>
-          <Link href="/login">
+          <Link href={redirect}>
             <Button
               className={cn(
                 "hidden md:block text-xl md:text-base mt-4 hover:bg-primary/95",
