5e81fe8e934b964a2cd5b98cca1928371c772a17
Akshay Bhatnagar <76173232+Astrra@users.noreply.github.com>
2023-11-25 22:05:20 +0530

    fixed type of params

--- app/(dashboard)/team/display/[slug]/page.tsx
+++ app/(dashboard)/team/display/[slug]/page.tsx
@@ -1,31 +1,26 @@
-import UserCard from '@/components/custom/user-card';
-import { createClient } from '@/utils/supabase/server';
-import { cookies } from 'next/headers';
-import React from 'react'
+import UserCard from "@/components/custom/user-card";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+import React from "react";
 
-const page = async ({params}) => {
+const page = async ({ params }: { params: { slug: string } }) => {
   const cookieStore = cookies();
   const supabase = createClient(cookieStore);
 
   const teamId = params.slug;
 
-  const { data: members} = await supabase
-  .from("team_members")
-  .select("*")
-  .eq("team_id",teamId);
-  
-  return (
-    <div className='ml-5 flex flex-wrap'>
-      {
-        members?.map((member) => (
-          <UserCard 
-            Id ={member.user_id}
-            ImageSrc="https://picsum.photos/200"          
-          />
-        ))
-      }
-    </div>
-  )
-}
+  const { data: members } = await supabase
+    .from("team_members")
+    .select("*")
+    .eq("team_id", teamId);
 
-export default page
+  return (
+    <div className="ml-5 flex flex-wrap">
+      {members?.map((member) => (
+        <UserCard Id={member.user_id} ImageSrc="https://picsum.photos/200" />
+      ))}
+    </div>
+  );
+};
+
+export default page;
